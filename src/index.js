2ea6290a818a6008100de4f6665685e14360b044
Gyanu Kumari Ranjeet Singh <gyanukumari.singh@sonata-software.com>
2023-09-05 15:15:43 +0530

    modified files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = ()=>{
-  return (
-  <div>
-    <SeasonDisplay/>
-  </div>
-    );
+class App extends Component{
+
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+          (position)=>{console.log(position)},
+          (error)=>{console.log(error)}
+        );
+
+        return(
+          <div>
+            <SeasonDisplay/>
+          </div>
+        )
+
+  }
 }
+
+// const App = ()=>{
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position)=>{console.log(position)},
+//     (error)=>{console.log(error)}
+//   );
+  
+//   return (
+//   <div>
+//     <SeasonDisplay/>
+//   </div>
+//     );
+// }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
